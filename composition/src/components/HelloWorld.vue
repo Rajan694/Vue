00fbb67fdf85546ed8621c46927d74ce59a1e6f8
Rajan694 <rajankushwaha694@gmail.com>
2023-09-28 11:32:17 +0530

    watchers

--- composition/src/components/HelloWorld.vue
+++ composition/src/components/HelloWorld.vue
@@ -1,11 +1,98 @@
 <template>
-  <h2>Home</h2>
+  <div class="counter">
+    <button class="counter-btn rounded-pill" @click="increment(2)">++</button>
+    <button class="counter-btn rounded-pill" @click="increment(1)">+</button>
+    <p class="counter-text">Counter: {{ counter }} |</p>
+    <p class="counter-text">| Counter: {{ counterData.count }}</p>
+
+    <button class="counter-btn rounded-pill" @click="decrement(1)">-</button>
+    <button class="counter-btn rounded-pill" @click="decrement(2)">--</button>
+  </div>
+  <br /><br />
+  <h4>The given number is {{ oddoreven }}</h4>
 </template>
 
-<script>
-export default {
-  name: "HelloWorld",
+<script setup>
+import { ref, reactive, computed, watch } from "vue";
+
+const counter = ref(0);
+const counterData = reactive({
+  count: 0,
+});
+watch(counter, (newValue, oldValue) => {
+  console.log(`The state changed from ${oldValue} to ${newValue}`);
+});
+watch(
+  () => counterData.count,
+  (newValue, oldValue) => {
+    console.log(`The state changed from ${oldValue} to ${newValue}`);
+  }
+);
+
+const oddoreven = computed(() => {
+  return counter.value % 2 === 0 ? "Even" : "Odd";
+});
+const increment = (amount) => {
+  counter.value += amount;
+  counterData.count += amount;
+};
+
+const decrement = (amount) => {
+  counter.value -= amount;
+  counterData.count -= amount;
 };
 </script>
 
-<style scoped></style>
+<!-- Older composition API 
+
+<script>
+import { ref } from "vue";
+
+export default {
+  setup() {
+    const counter = ref(0);
+
+    const increment = () => {
+      counter.value++;
+    };
+
+    const decrement = () => {
+      counter.value--;
+    };
+
+    return {
+      counter,
+      increment,
+      decrement,
+    };
+  },
+};
+</script> -->
+
+<style>
+.counter {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin-top: 20px;
+}
+
+.counter-text {
+  font-size: 24px;
+  margin-bottom: 10px;
+}
+
+.counter-btn {
+  padding: 8px 16px;
+  background-color: #007bff;
+  color: #fff;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  margin: 5px;
+}
+
+.counter-btn:hover {
+  background-color: #0056b3;
+}
+</style>
--- composition/src/views/HomeView.vue
+++ composition/src/views/HomeView.vue
@@ -1,9 +1,10 @@
 <template>
-  <HelloWorld />
+  <div class="container">
+    <HelloWorld />
+  </div>
 </template>
 
 <script>
-// @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
