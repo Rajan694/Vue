7820833ce358772f5236cf16528d40305eae4c17
Rajan694 <rajankushwaha694@gmail.com>
2023-09-28 14:03:09 +0530

    refs

--- composition/src/views/AboutView.vue
+++ composition/src/views/AboutView.vue
@@ -2,6 +2,8 @@
   <div class="about">
     <h1>This is an about page</h1>
     <input v-autofocus />
+    <input ref="input" />
+    <button @click="getd()">Focus</button>
   </div>
   <ul>
     <li v-for="item in details" :key="item.id">
@@ -11,6 +13,11 @@
 </template>
 <script setup>
 import { ref } from "vue";
+
+const input = ref(null);
+const getd = () => {
+  input.value.focus();
+};
 
 const details = ref([
   { id: 1, title: "About 1" },
