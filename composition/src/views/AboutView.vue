e28cfcca69885ef8369a55115c128e476ccd4f39
Rajan694 <rajankushwaha694@gmail.com>
2023-09-28 12:05:27 +0530

    autofocus directive

--- composition/src/views/AboutView.vue
+++ composition/src/views/AboutView.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+
+    <input v-autofocus />
   </div>
 </template>
+<script setup>
+const vAutofocus = {
+  // use it as v-autofocus
+  mounted: (el) => {
+    el.focus();
+  },
+};
+</script>
